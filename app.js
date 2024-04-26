2de89856024cc085dcd263f4351da7b5b18628c3
numanarshad123 <154283305+numanarshad123@users.noreply.github.com>
2024-04-26 05:52:11 +0500

    complete

---
+++ app.js
@@ -0,0 +1,91 @@
+
+
+// //  Que no 1
+
+// var array = [1,2,3,4,5]
+
+// for (var i=0; i< array.length; i++){
+//     if (array[i]%2 !=0){
+//         array[i] = array[i]*2;
+//     }
+// }
+// console.log(array);
+
+// // Que no 2
+
+// var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+// var number = '1234567890'
+// var password = prompt('Password')
+
+// var minLength = false
+// var alphabet = false
+// var number =false
+
+// if (password.length >=6) minLength true
+// if (!isNaN(password[0])) password = true 
+
+// for (var i=o; i<password.length; i++){
+//     console.log(password[i])
+//     if (alphabet.indexOf(password[i]) !== -1){
+//         alphabet = true
+//     }
+//     if (number.indexOf(password[i] !== -1)){
+//         number = true 
+//     }
+// }
+//  if (password && minLength && alphabet && number){
+//     alert( 'Password is correct')
+//  }else {
+//     alert('Password is not Correct')
+//  }
+
+// // Que no 3 
+
+// var a = [1,2,3,4,5,6,7,8,9,10]
+// var b = [3,6,7,9,10,35,3573];
+// var fresh = []
+
+// for (var i=o; i <a.length; i++){
+//     for(var j=o; j<b.length; j++){
+//         if (a[i]==b[j]){
+//             fresh.push(a[i])
+//         }
+//     }
+// }
+// console.log(fresh)
+
+// //  Que no 4
+
+// var number = parseInt(prompt("Enter a Number"));
+// var isprime = true;
+
+// if (number===1){
+//     console.log("1 is not a prime number");
+// }
+// else if (number > 1){
+//     for (var i=0; i <number; i++){
+//         if (number% i ==0){
+//             isprime = false
+//             break
+//         }
+//     }
+// }
+
+// if (isprime){
+//     console.log(`${number}is a prime number`);
+// }else if {
+//     console.log(`${number}is not a prime number`);
+// }else  {
+//     console.log("Number is not a prime Number");
+// }
+
+// //  Que no 5
+
+// var arr = [[1,2],[3,4],[5,6],[7,8,9],[10,11,12,13,14,15]];
+
+// var flatArray = [].concat.apply([],arr );
+// console,log(flatArray
+// )
+
+
+//  STRING PROBLEMS /// METHODS///////
